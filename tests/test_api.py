f66290fb96203c4589290b799d1b5df5431b7a41
David Puerta Martín <puertadavid96182@gmail.com>
2023-04-19 03:17:33 -0400

    improve imports in tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,5 @@
-import sys
-import os
-
-import requests
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import unittest
+import requests
 from unittest.mock import patch
 from app import create_app
 import json
