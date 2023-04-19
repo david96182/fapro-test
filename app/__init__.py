adc81c1c5826c1592d60ec58928f0cd177aad4a1
David Puerta Martín <puertadavid96182@gmail.com>
2023-04-19 00:38:55 -0400

    update endpoint and add error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from .endpoints.get_value_by_date import get_date_by_date
+from .endpoints.get_value_by_date import get_value_by_date
 
 
 def create_app():
     app = Flask(__name__)
 
     # register the endpoints
-    app.register_blueprint(get_date_by_date)
+    app.register_blueprint(get_value_by_date)
 
     return app
--- app/endpoints/get_value_by_date.py
+++ app/endpoints/get_value_by_date.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, jsonify
+import datetime
+from flask import Blueprint, jsonify, request
 
-get_date_by_date = Blueprint('get_date_by_date', __name__)
+get_value_by_date = Blueprint('get_value_by_date', __name__)
 
 
-@get_date_by_date.route('/api/date')
-def date1():
-    return jsonify({'date': '123'})
+@get_value_by_date.route('/api/date/<string:date_string>')
+def get_value(date_string):
+    if not date_string:
+        return jsonify({'error': 'DATE_MISSING', 'message': 'Date is a required parameter.'}), 400
+
+    try:
+        # Validate date format and check if it exists
+        date = datetime.datetime.strptime(date_string, '%d-%m-%Y')
+        if date.year < 2013 or date > datetime.datetime.now():
+            return jsonify({'error': 'DATE_INVALID_RANGE',
+                            'message': 'Date must be greater than 01-01-2013 and less than today.'}), 400
+    except ValueError:
+        return jsonify({'error': 'DATE_INVALID_FORMAT', 'message': 'Date format must be DD-MM-YYYY.'}), 400
+
+    # If date validation passes, return the date value
+    return jsonify({'date': date_string})
